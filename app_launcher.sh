8dea74204fce573347c8a27baa401d5dcf100aa1
Kevin W Palmer <kevinwpalmer@gmail.com>
2015-06-02 01:39:17 +0000

    New stopping point

--- app_launcher.sh
+++ app_launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /
-cd /home/pi
+cd /home/pi/gsd/videoPython
 sudo python get_http.py
 cd /
--- get_http.py
+++ get_http.py
@@ -1,8 +1,12 @@
 #! /usr/bin/env python
+
 import os
 import urllib2
 import json
-os.environ['http_proxy'] = ''
+import subprocess
+import StringIO
+
+#os.environ['http_proxy'] = ''
 kp = ""
 eth0mac = open('/sys/class/net/eth0/address')
 wlan0mac = open('/sys/class/net/wlan0/address')
@@ -12,8 +16,9 @@ print wlan0
 print eth0
 
 kp = 'http://greensnakedesign.com/michael/pitest.php?mac=%s' %(eth0)
+#kp = 'http:
 print kp
-request = urllib2.Request(kp)
+#request = urllib2.Request(kp)
 
 response = urllib2.urlopen(kp)
 
@@ -24,8 +29,17 @@ print html
 js =  json.loads(html)
 print js
 dump =  json.dumps(html)
+
 if js['status'][0] == 'good':
 	print js['status'][0]
 else:
 	print "not good"
-#
+
+p = subprocess.Popen("date", stdout = subprocess.PIPE)
+output, err = p.communicate()
+jd = json.dumps(output)
+jl = json.loads(jd)
+jls = jl.split()
+
+print jls[0]
+
